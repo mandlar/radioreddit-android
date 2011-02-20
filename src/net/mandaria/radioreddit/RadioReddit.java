15722355e1f3e3e30fe7092c825c08a6a64b8e9c
mandlar <mandlar@users.noreply.github.com>
2011-02-20 21:09:46 +0000

    Very simple media player to access main radioreddit stream. This will only work for Android 2.2+, will have to look into using a StreamProxy:
    http://code.google.com/p/npr-android-app/source/browse/trunk/Npr/src/org/npr/android/news/StreamProxy.java
    
    http://stackoverflow.com/questions/4557450/audio-stream-buffering

--- src/net/mandaria/radioreddit/RadioReddit.java
+++ src/net/mandaria/radioreddit/RadioReddit.java
@@ -1,13 +1,80 @@
 package net.mandaria.radioreddit;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.IOException;
 
-public class RadioReddit extends Activity {
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class RadioReddit extends Activity 
+{
+	MediaPlayer mediaPlayer = new MediaPlayer();;
+	Button btn_play;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+
+        
+        
+        btn_play = (Button)findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{		
+				if (mediaPlayer.isPlaying()) 
+				{
+					mediaPlayer.stop();
+					btn_play.setText("Play");
+				} 
+				else 
+				{
+					mediaPlayer.reset();
+					try 
+					{
+						mediaPlayer.setDataSource("http://texas.radioreddit.com:8000/");
+					} 
+					catch (IllegalArgumentException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+					catch (IllegalStateException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				       
+					try 
+					{
+						mediaPlayer.prepare();
+					} 
+					catch (IllegalStateException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+					catch (IOException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					mediaPlayer.start();
+					btn_play.setText("Stop");
+				}
+			}
+		});
     }
 }
