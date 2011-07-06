ef8a42a0a9e4d9f0b8037623e69888a04d905bdf
Mandlar <Mandlar@ca3eee7c-591e-30bd-a63d-68a7c02126da>
2011-07-06 01:36:40 +0000

    Cleaned up code formatting, removed old TODO

--- trunk/RadioReddit/src/net/mandaria/radioreddit/RadioRedditApplication.java
+++ trunk/RadioReddit/src/net/mandaria/radioreddit/RadioRedditApplication.java
@@ -1,4 +1,5 @@
 package net.mandaria.radioreddit;
+
 import java.util.ArrayList;
 
 import net.mandaria.radioreddit.errors.CustomExceptionHandler;
@@ -7,19 +8,18 @@ import net.mandaria.radioreddit.objects.RadioSong;
 import net.mandaria.radioreddit.objects.RadioStream;
 import android.app.Application;
 
-public class RadioRedditApplication extends Application 
+public class RadioRedditApplication extends Application
 {
-	//public String current_station = "main"; // TODO: don't hard code "main" here, pull it programatically
 	public RadioStream CurrentStream;
 	public RadioSong CurrentSong;
 	public RadioEpisode CurrentEpisode;
 	public ArrayList<RadioStream> RadioStreams;
-	
+
 	@Override
-	public void onCreate() 
+	public void onCreate()
 	{
 		Thread.setDefaultUncaughtExceptionHandler(new CustomExceptionHandler(getApplicationContext()));
-		
-		super.onCreate();	 
+
+		super.onCreate();
 	}
 }
