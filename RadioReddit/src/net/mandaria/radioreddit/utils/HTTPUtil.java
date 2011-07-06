f72464c1a18626b2b4405fd4db4b2fa66081d78c
Mandlar <mandlar@users.noreply.github.com>
2011-07-06 03:11:56 +0000

    Cleaned up code

--- RadioReddit/src/net/mandaria/radioreddit/utils/HTTPUtil.java
+++ RadioReddit/src/net/mandaria/radioreddit/utils/HTTPUtil.java
@@ -16,63 +16,63 @@ import android.net.Uri;
 public class HTTPUtil
 {
 
-	public static String slurp (InputStream in) throws IOException 
+	public static String slurp(InputStream in) throws IOException
 	{
-	    StringBuffer out = new StringBuffer();
-	    byte[] b = new byte[4096];
-	    for (int n; (n = in.read(b)) != -1;) 
-	    {
-	        out.append(new String(b, 0, n));
-	    }
-	    return out.toString();
+		StringBuffer out = new StringBuffer();
+		byte[] b = new byte[4096];
+		for(int n; (n = in.read(b)) != -1;)
+		{
+			out.append(new String(b, 0, n));
+		}
+		return out.toString();
 	}
-	
+
 	// gets http output from URL
-	public static String get(Context c, String url) throws ClientProtocolException, IOException 
+	public static String get(Context c, String url) throws ClientProtocolException, IOException
 	{
-       HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-       //conn.setRequestProperty("User-Agent", Utils.USER_AGENT);
+		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+		// conn.setRequestProperty("User-Agent", Utils.USER_AGENT);
 
-       // Account a = Account.getActiveAccount(c); // Account is just a class to hold the reddit login cookie
-       //if(a != null) 
-       //{
-       //	conn.setRequestProperty("Cookie", "reddit_session=" + a.redditSession); // I tried sending cookies in other ways but none worked on Android 2.1 or 2.2 except this
-       //}
+		// Account a = Account.getActiveAccount(c); // Account is just a class to hold the reddit login cookie
+		// if(a != null)
+		// {
+		// conn.setRequestProperty("Cookie", "reddit_session=" + a.redditSession); // I tried sending cookies in other ways but none worked on Android 2.1 or 2.2 except this
+		// }
 
-       String output = HTTPUtil.slurp(conn.getInputStream());
-       conn.getInputStream().close();
+		String output = HTTPUtil.slurp(conn.getInputStream());
+		conn.getInputStream().close();
 
-       return output;
+		return output;
 	}
-	
+
 	// posts data to http, gets output from URL
-	public static String post(Context c, String url, List<NameValuePair> params) throws ClientProtocolException, IOException 
+	public static String post(Context c, String url, List<NameValuePair> params) throws ClientProtocolException, IOException
 	{
-	   StringBuilder post = new StringBuilder(); // Using the built in method to post a List<NameValuePair> fails on Android 2.1 / 2.2.... manually post the data
-       for(NameValuePair p : params) 
-       {
-    	   post.append(Uri.encode(p.getName()) + "=" + Uri.encode(p.getValue()) + "&");
-       }
-       post.deleteCharAt(post.length() - 1); // Remove trailing &
+		StringBuilder post = new StringBuilder(); // Using the built in method to post a List<NameValuePair> fails on Android 2.1 / 2.2.... manually post the data
+		for(NameValuePair p : params)
+		{
+			post.append(Uri.encode(p.getName()) + "=" + Uri.encode(p.getValue()) + "&");
+		}
+		post.deleteCharAt(post.length() - 1); // Remove trailing &
 
-       HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-       conn.setDoOutput(true);
-       //conn.setRequestProperty("User-Agent", Utils.USER_AGENT);
-       conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-       conn.setRequestProperty("Content-Length", Integer.toString(post.length()));
+		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+		conn.setDoOutput(true);
+		// conn.setRequestProperty("User-Agent", Utils.USER_AGENT);
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.setRequestProperty("Content-Length", Integer.toString(post.length()));
 
-//       Account a = Account.getActiveAccount(c);
-//       if(a != null) 
-//       {
-//    	   conn.setRequestProperty("Cookie", "reddit_session=" + a.redditSession);
-//       }
+		// Account a = Account.getActiveAccount(c);
+		// if(a != null)
+		// {
+		// conn.setRequestProperty("Cookie", "reddit_session=" + a.redditSession);
+		// }
 
-       OutputStream os = conn.getOutputStream();
-       os.write(post.toString().getBytes());
+		OutputStream os = conn.getOutputStream();
+		os.write(post.toString().getBytes());
 
-       String output = HTTPUtil.slurp(conn.getInputStream());
-       conn.getInputStream().close();
+		String output = HTTPUtil.slurp(conn.getInputStream());
+		conn.getInputStream().close();
 
-       return output;
-   }
+		return output;
+	}
 }
