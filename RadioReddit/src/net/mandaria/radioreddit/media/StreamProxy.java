55d2d9d8d7ea8d4015d8121779099e0dad44e346
Mandlar <mandlar@users.noreply.github.com>
2011-07-06 03:43:15 +0000

    Added GPL license.txt and header to all source code files

--- RadioReddit/src/net/mandaria/radioreddit/media/StreamProxy.java
+++ RadioReddit/src/net/mandaria/radioreddit/media/StreamProxy.java
@@ -1,16 +1,25 @@
-// Copyright 2010 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ *	radio reddit for android: mobile app to listen to radioreddit.com
+ *  Copyright (C) 2011 Bryan Denny
+ *  
+ *  This file is part of "radio reddit for android"
+ *
+ *  "radio reddit for android" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  "radio reddit for android" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with "radio reddit for android".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// This file was derived from NPR's StreamProxy which is licensed under Apache License, Version 2.0
+// You can find the original file here: https://code.google.com/p/npr-android-app/source/browse/trunk/Npr/src/org/npr/android/news/StreamProxy.java
 
 package net.mandaria.radioreddit.media;
 
