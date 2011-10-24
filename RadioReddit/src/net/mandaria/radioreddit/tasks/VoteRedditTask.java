70f415fb169bfeb422cdd525c6696606ff6e1d58
mandlar <mandlar@users.noreply.github.com>
2011-10-24 13:11:22 +0000

    Added reddit vote function, initial (incomplete) setup of voting on a song, added async task to run the API call, added click listeners to upvote and downvote buttons that call this basic API

---
+++ RadioReddit/src/net/mandaria/radioreddit/tasks/VoteRedditTask.java
@@ -0,0 +1,93 @@
+package net.mandaria.radioreddit.tasks;
+
+import java.util.Locale;
+
+import net.mandaria.radioreddit.RadioRedditApplication;
+import net.mandaria.radioreddit.activities.Login;
+import net.mandaria.radioreddit.activities.RadioReddit;
+import net.mandaria.radioreddit.activities.Settings;
+import net.mandaria.radioreddit.apis.RadioRedditAPI;
+import net.mandaria.radioreddit.apis.RedditAPI;
+import net.mandaria.radioreddit.objects.RadioEpisode;
+import net.mandaria.radioreddit.objects.RedditAccount;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+public class VoteRedditTask extends AsyncTask<Void, String, String>
+{
+	private static String TAG = "RadioReddit";
+	private Context _context;
+	private RadioRedditApplication _application;
+	private Exception ex;
+	private boolean _liked;
+	private ProgressDialog _progressDialog;
+
+	public VoteRedditTask(RadioRedditApplication application, Context context, boolean liked)
+	{
+		_context = context;
+		_application = application;
+		_liked = liked;
+		// TODO: probably shouldn't show a dialog?
+		_progressDialog = ProgressDialog.show(_context, "Voting on currently playing...", "Please wait...", true);
+	}
+
+	@Override
+	protected String doInBackground(Void... unused)
+	{
+		String errorMessage = "";
+		try
+		{
+			errorMessage = RadioRedditAPI.VoteOnCurrentlyPlaying(_context, _application, _liked);
+		}
+		catch(Exception e)
+		{
+			ex = e;
+			Log.e(TAG, "FAIL: voting on currently playing: " + e);
+		}
+
+		return errorMessage;
+	}
+
+	@Override
+	protected void onProgressUpdate(String... item)
+	{
+
+	}
+
+	@Override
+	protected void onPostExecute(String result)
+	{
+		_progressDialog.dismiss();
+		if(result != null && result.equals(""))
+		{
+			// TODO: hide this later, don't show to user
+			Toast.makeText(_context, "Successfully voted!", Toast.LENGTH_LONG).show();
+		}
+		else
+		{
+			if(result != null)
+			{
+				final AlertDialog.Builder builder = new AlertDialog.Builder(_context);
+			    builder.setMessage("Error: " + result)
+			    	.setTitle("Voting Error")
+			    	.setIcon(android.R.drawable.ic_dialog_alert)
+			    	.setCancelable(true)
+			        .setPositiveButton("OK", null);
+			    
+			    final AlertDialog alert = builder.create();
+			    alert.show();
+			    
+				Log.e(TAG, "FAIL: Post execute: " + result);
+			}
+		}
+
+		if(ex != null)
+			Log.e(TAG, "FAIL: EXCEPTION: Post execute: " + ex);
+
+	}
+}
