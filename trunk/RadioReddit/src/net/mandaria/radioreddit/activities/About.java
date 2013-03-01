76fc7e43b31dc693fdce6966f62943c9202b0ed7
mandlar <mandlar@ca3eee7c-591e-30bd-a63d-68a7c02126da>
2013-03-01 01:40:50 +0000

    Added About activity (stubbed/empty)

---
+++ trunk/RadioReddit/src/net/mandaria/radioreddit/activities/About.java
@@ -0,0 +1,75 @@
+/*
+ *	radio reddit for android: mobile app to listen to radioreddit.com
+ *  Copyright (C) 2011 Bryan Denny
+ *  
+ *  This file is part of "radio reddit for android"
+ *
+ *  "radio reddit for android" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  "radio reddit for android" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with "radio reddit for android".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.mandaria.radioreddit.activities;
+
+import net.mandaria.radioreddit.R;
+import net.mandaria.radioreddit.RadioRedditApplication;
+import net.mandaria.radioreddit.apis.RedditAPI;
+import net.mandaria.radioreddit.tasks.LoginRedditTask;
+import net.mandaria.radioreddit.utils.ActivityUtil;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.flurry.android.FlurryAgent;
+
+public class About extends SherlockActivity
+{
+	Button btn_login;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{	
+		super.onCreate(savedInstanceState);
+		
+		ActivityUtil.SetKeepScreenOn(this);
+		
+		setContentView(R.layout.about);
+		getSupportActionBar().setTitle(getString(R.string.about));
+	}
+	
+	@Override
+	public void onStart()
+    {
+       super.onStart();
+       FlurryAgent.onStartSession(this, getString(R.string.flurrykey));
+    }
+    
+    @Override
+	public void onStop()
+    {
+       super.onStop();
+       FlurryAgent.onEndSession(this);
+    }
+    
+}
